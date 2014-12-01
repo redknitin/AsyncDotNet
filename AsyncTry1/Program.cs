a9fbc915f07e7665517721e5af1569425e7dd7ec
Nitin Reddy <k_nitin_r@yahoo.co.in>
2014-12-01 13:41:36 +0400

    Committed working sample.

---
+++ AsyncTry1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTry1
+{
+    class Program
+    {
+        static async Task<bool> DoSomething()
+        {
+            await Task.Run(() => { System.Threading.Thread.Sleep(5000); Console.WriteLine("Hello"); });            
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            var d = DoSomething();
+            Console.WriteLine("Done thing");
+            d.Wait();
+            Console.Read();
+        }
+    }
+}
